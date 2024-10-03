a82e5258926bfdf899ffc00ab9ae09e513d28c41
Gabriel Dutra <gabrieldutradev@gmail.com>
2024-10-03 10:15:27 -0300

    Adicionado método que salva dados usuário

---
+++ src/main/java/com/javanauta/usuario/business/converter/UsuarioConverter.java
@@ -0,0 +1,100 @@
+package com.javanauta.usuario.business.converter;
+
+import com.javanauta.usuario.business.dto.EnderecoDTO;
+import com.javanauta.usuario.business.dto.TelefoneDTO;
+import com.javanauta.usuario.business.dto.UsuarioDTO;
+import com.javanauta.usuario.infrastructure.entity.Endereco;
+import com.javanauta.usuario.infrastructure.entity.Telefone;
+import com.javanauta.usuario.infrastructure.entity.Usuario;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UsuarioConverter {
+
+    //Parte do código que converte de DTO para Entity
+    public Usuario paraUsuario(UsuarioDTO usuarioDTO) {
+        return Usuario.builder()
+                .nome(usuarioDTO.getNome())
+                .email(usuarioDTO.getEmail())
+                .senha(usuarioDTO.getSenha())
+                .enderecos(paraListaEndereco(usuarioDTO.getEnderecos()))
+                .telefones(paraListaTelefone(usuarioDTO.getTelefones()))
+                .build();
+    }
+
+    public List<Endereco> paraListaEndereco(List<EnderecoDTO> enderecoDTOS) {
+        List<Endereco> enderecos = new ArrayList<>();
+        for (EnderecoDTO enderecoDTO : enderecoDTOS) {
+            enderecos.add(paraEndereco(enderecoDTO));
+        }
+        return enderecos;
+        //return enderecoDTOS.stream().map(this::paraEndereco).toList();
+    }
+
+    public Endereco paraEndereco(EnderecoDTO enderecoDTO) {
+        return Endereco.builder()
+                .rua(enderecoDTO.getRua())
+                .numero(enderecoDTO.getNumero())
+                .complemento(enderecoDTO.getComplemento())
+                .cidade(enderecoDTO.getCidade())
+                .cep(enderecoDTO.getCep())
+                .estado(enderecoDTO.getEstado())
+                .build();
+    }
+
+    public List<Telefone> paraListaTelefone(List<TelefoneDTO> telefoneDTOS) {
+        return telefoneDTOS.stream().map(this::paraTelefone).toList();
+    }
+
+    public Telefone paraTelefone(TelefoneDTO telefoneDTO) {
+        return Telefone.builder()
+                .numero(telefoneDTO.getNumero())
+                .ddd(telefoneDTO.getDdd())
+                .build();
+    }
+
+    //Parte do código que converte de Entity para DTO
+    public UsuarioDTO paraUsuarioDTO(Usuario usuario) {
+        return UsuarioDTO.builder()
+                .nome(usuario.getNome())
+                .email(usuario.getEmail())
+                .senha(usuario.getSenha())
+                .enderecos(paraListaEnderecoDTO(usuario.getEnderecos()))
+                .telefones(paraListaTelefoneDTO(usuario.getTelefones()))
+                .build();
+    }
+
+    public List<EnderecoDTO> paraListaEnderecoDTO(List<Endereco> enderecos) {
+        List<EnderecoDTO> enderecosDTO = new ArrayList<>();
+        for (Endereco endereco : enderecos) {
+            enderecosDTO.add(paraEnderecoDTO(endereco));
+        }
+        return enderecosDTO;
+        //return enderecoDTOS.stream().map(this::paraEndereco).toList();
+    }
+
+    public EnderecoDTO paraEnderecoDTO(Endereco endereco) {
+        return EnderecoDTO.builder()
+                .rua(endereco.getRua())
+                .numero(endereco.getNumero())
+                .complemento(endereco.getComplemento())
+                .cidade(endereco.getCidade())
+                .cep(endereco.getCep())
+                .estado(endereco.getEstado())
+                .build();
+    }
+
+    public List<TelefoneDTO> paraListaTelefoneDTO(List<Telefone> telefones) {
+        return telefones.stream().map(this::paraTelefoneDTO).toList();
+    }
+
+    public TelefoneDTO paraTelefoneDTO(Telefone telefone) {
+        return TelefoneDTO.builder()
+                .numero(telefone.getNumero())
+                .ddd(telefone.getDdd())
+                .build();
+    }
+}
